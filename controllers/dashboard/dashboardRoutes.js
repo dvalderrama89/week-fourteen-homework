7673e85029b520789f3a3b6fa08e35c77b7b5bb4
David Valderrama <dvalderrama08@gmail.com>
2021-06-22 18:24:50 -0700

    Added create view and momentjs for post model. Fixed dashboard query.

--- controllers/dashboard/dashboardRoutes.js
+++ controllers/dashboard/dashboardRoutes.js
@@ -1,12 +1,14 @@
 const router = require('express').Router();
 const withAuth = require('../../utils/auth');
 const Post = require('../../models/Post');
+let moment = require('moment');
 
 router.get('/', withAuth, async (req, res) => {
+    console.log('dashboard get route');
     try {
         const postData = await Post.findAll({
             where: {
-                id: req.session.user_id
+                author_id: req.session.user_id
             }
         });
 
@@ -25,6 +27,23 @@ router.get('/create', withAuth, (req, res) => {
     res.render('create', {
         loggedIn: req.session.loggedIn,
     });
+});
+
+router.post('/create', withAuth, async (req, res) => {
+    try 
+    {
+        currentDatetime =  moment(new Date()).format("YYYY-MM-DD HH:mm:ss");
+        const postData = await Post.create({
+            post_title: req.body.title,
+            post_body: req.body.postBody,
+            author_id: req.session.user_id,
+            created_at: currentDatetime,
+            updated_at: currentDatetime,
+        });
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+    }
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -46,6 +46,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(routes); 
 
 // Syncs sequelize
-sequelize.sync({ force: true }).then(() => {
+sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
 });
